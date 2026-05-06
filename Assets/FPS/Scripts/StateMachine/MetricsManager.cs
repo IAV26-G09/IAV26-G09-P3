62053d8a1915ff0796bfb90d80bc6f17447f0a08
nievesag <nievalon@ucm.es>
2026-05-06 23:01:07 +0200

    fps

--- Assets/FPS/Scripts/StateMachine/MetricsManager.cs
+++ Assets/FPS/Scripts/StateMachine/MetricsManager.cs
@@ -10,9 +10,20 @@ namespace IAV26.G09.P3
     {
         [SerializeField] private TMPro.TMP_Text muertes; // veces que se ha muerto
         [SerializeField] private TMPro.TMP_Text asesinatos; // veces que ha matado
+        [SerializeField] private TMPro.TMP_Text fps; // fps
 
+        // muertes/asesinatos
         int m_Muertes;
         int m_Asesinatos;
+
+        // fps
+        private int frameRate = 60;
+
+        // Variables de timer de framerate
+        int m_frameCounter = 0;
+        float m_timeCounter = 0.0f;
+        float m_lastFramerate = 0.0f;
+        float m_refreshTime = 0.5f;
 
         private void Awake()
         {
@@ -21,6 +32,47 @@ namespace IAV26.G09.P3
 
             m_Muertes = 0;
             m_Asesinatos = 0;
+        }
+
+        private void Start()
+        {
+            Application.targetFrameRate = frameRate;
+        }
+
+        private void Update()
+        {
+            // Timer para mostrar el frameRate a intervalos
+            if (m_timeCounter < m_refreshTime)
+            {
+                m_timeCounter += Time.deltaTime;
+                m_frameCounter++;
+            }
+            else
+            {
+                m_lastFramerate = (float)m_frameCounter / m_timeCounter;
+                m_frameCounter = 0;
+                m_timeCounter = 0.0f;
+            }
+
+            // Texto con el framerate y 2 decimales
+            if (fps != null)
+            {
+                fps.text = (((int)(m_lastFramerate * 100 + .5) / 100.0)).ToString("N0");
+            }
+        }
+
+        private void ChangeFrameRate()
+        {
+            if (frameRate == 30)
+            {
+                frameRate = 60;
+                Application.targetFrameRate = 60;
+            }
+            else
+            {
+                frameRate = 30;
+                Application.targetFrameRate = 30;
+            }
         }
 
         void OnPlayerDeath(PlayerDeathEvent evt)
